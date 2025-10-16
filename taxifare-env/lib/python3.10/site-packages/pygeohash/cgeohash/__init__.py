a79ed673c805baf0ab089eb01edb8c23e5d65422
Shahad It <shahadalrehily@gmail.com>
2025-10-16 10:52:45 +0000

    chore: remove large virtual environment folder

--- taxifare-env/lib/python3.10/site-packages/pygeohash/cgeohash/__init__.py
+++
@@ -1,6 +0,0 @@
-"""C-extension implementation of geohash encoding and decoding.
-
-This module provides a high-performance C implementation of geohash
-encoding and decoding functionality, serving as a drop-in replacement
-for the original Python implementation.
-"""
